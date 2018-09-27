6f9550bc6b29d623c164080641d5a9e80f1e99ab
silver2row <silver1row@cox.net>
2018-09-26 19:33:21 -0500

    making things easy to understand...

--- software/RobotFly.py
+++ software/RobotFly.py
@@ -2,9 +2,18 @@
 
 from pysabertooth import Sabertooth
 from flask import Flask, render_template
+import serial
 import time
 
-saber = Sabertooth("/dev/ttyO1", baudrate=9600, address=128, timeout=0.1)
+# on the beaglebone related boards, use P9_21 for UART2 and set up config-pin.
+
+# attach P9_21 to S1 on the sabertooth 2 x 12 and attach GND on the sabertooth to GND on your BBB.
+
+saber = Sabertooth("/dev/ttyO2", baudrate=9600, address=128, timeout=0.1)
+
+# for instance, sudo config-pin P9.21 uart, that cmd will do it.
+
+# start and enable a service, start a cron job, or make an executable .sh file.
 
 app = Flask(__name__)
 @app.route("/")
@@ -15,23 +24,23 @@ def updates(state=None):
         print "Robot Moving Forward"
         saber.drive(1, 100)
         saber.drive(2, 100)
-        time.sleep(.2)
+        #time.sleep(.2)
     if state == "R":
         print "Robot Turning Right"
         saber.drive(1, 75)
         saber.drive(2, 25)
-        time.sleep(.2)
+        #time.sleep(.2)
     if state == "L":
         print "Robot Turning Left"
         saber.drive(1, 25)
         saber.drive(2, 75)
-        time.sleep(.2)
+        #time.sleep(.2)
     if state == "S":
         print "Robot Stopped"
         saber.drive(1, 0)
         saber.drive(2, 0)
         saber.stop()
-        time.sleep(.2)
+        #time.sleep(.2)
     template_data = {
         "title" : state,
     }
